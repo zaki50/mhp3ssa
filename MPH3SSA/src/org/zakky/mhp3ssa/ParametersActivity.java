8bc7a66104e7750ce0dd39e7d02b75d415c56aae
YAMAZAKI Makoto <makoto1975@gmail.com>
2010-12-09 16:06:04 +0900

    - Android アプリ用のプロジェクトを作成しました。

---
+++ MPH3SSA/src/org/zakky/mhp3ssa/ParametersActivity.java
@@ -0,0 +1,13 @@
+package org.zakky.mhp3ssa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParametersActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
